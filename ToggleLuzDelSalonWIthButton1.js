df2acf3dc6a3559122a174ba6be87fdce0f2f69b
vcalatayud <lacuentadevictor@gmail.com>
2020-05-02 03:02:35 +0200

    fixed ipsolver path

--- ToggleLuzDelSalonWIthButton1.js
+++ ToggleLuzDelSalonWIthButton1.js
@@ -3,7 +3,7 @@ const XiaomiFactory = require('./adapters/XiaomiAdapters/XiaomiFactory');
 const DeviceRepository = require('./interface/DeviceRepository');
 const ToggleLightWithButton = require('./domain/use-case/ToggleLightWithButtonNotSynchronized');
 const devicesData = require('./interface/DevicesData')
-const IpSolver = require('../interface/ipSolver');
+const IpSolver = require('./interface/ipSolver');
 
 const tuyaFactory = TuyaFactory();
 const xiaomiFactory = XiaomiFactory();
